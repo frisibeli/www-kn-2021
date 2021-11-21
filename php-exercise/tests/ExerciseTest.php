e1097920482b82e6b2ee03ce7bddd3c5e2428838
Kristiyan Krumov <kristiyan.boyanov@gmail.com>
2021-11-22 00:55:42 +0200

    Re-run tests

--- php-exercise/tests/ExerciseTest.php
+++ php-exercise/tests/ExerciseTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 final class ExerciseTest extends TestCase {
     public function testSaySomething(): void {
-        $p = new Exercise("hah");
-        $this->assertEquals("Say something hah", $p->saySomething());
+        $p = new Exercise("haha");
+        $this->assertEquals("Say something haha", $p->saySomething());
     }
 }
